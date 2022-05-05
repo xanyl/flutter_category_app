c8d5a1f930a308b7b5b8b3ae687f714a02979a4e
xanyl <aniltiwari645@gmail.com>
2022-05-05 20:20:39 +0545

    Problem  in home_page.dart in initState()

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
+import 'dart:convert';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "AK";
+
   @override
+  void initState() {
+    loadData();
+  }
+
+  loadData() async {
+    var catalogjson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogjson);
+  }
+
   Widget build(BuildContext context) {
-    var list = List.generate(5, (index) => CatalogModel.items[0]);
+    var list = List.generate(10, (index) => CatalogModel.items[0]);
     final dummyList = list;
     return Scaffold(
       appBar: AppBar(
